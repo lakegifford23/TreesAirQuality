c6abbdec42a0b3006857f9de8ce67c8c0a9bd26b
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-11-21 19:55:53 -0500

    working on walking skeleton!

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let character_info = fs.readFileSync('data/CDdata.json', 'utf8');
+let character_info = fs.readFileSync('../data/CDdata.json', 'utf8');
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
-  data: JSON.parse(district_info)
+  data: JSON.parse(character_info)
 });
 
-fs.writeFileSync('build/index.html', index_html, 'utf8');
+fs.writeFileSync('../build/index.html', index_html, 'utf8');
