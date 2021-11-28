3f9bf04fbfb9c71bf139ba92a33828c5b3c3fb44
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-11-28 14:29:21 -0500

    got the about.html page to work!!

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 let character_info = fs.readFileSync('data/CDdata.json', 'utf8');
 let index_template = fs.readFileSync('src/views/index.ejs', 'utf8');
 let micro1 = fs.readFileSync('src/views/micro.ejs', 'utf8');
+let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
+
 
 
 let index_html = ejs.render(index_template, {
@@ -16,5 +18,11 @@ let micro_html = ejs.render(micro1, {
   data: JSON.parse(character_info)
 });
 
+let about_html = ejs.render(about_template, {
+  filename: __dirname + '/views/about.ejs',
+  data: JSON.parse(character_info)
+});
+
 fs.writeFileSync('build/index.html', index_html, 'utf8');
 fs.writeFileSync('build/micro.html', micro_html, 'utf8');
+fs.writeFileSync('build/about.html', about_html, 'utf8');
