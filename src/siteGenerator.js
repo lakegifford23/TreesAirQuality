108e9ecfdd15cd31c4ecf0ce5e76da3c3547c309
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-11-29 10:02:50 -0500

    tried to fix sitegenerator

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const ejs = require('ejs');
-
-let character_info = fs.readFileSync('data/CDdata.json', 'utf8');
 let index_template = fs.readFileSync('src/views/index.ejs', 'utf8');
 let micro1 = fs.readFileSync('src/views/micro.ejs', 'utf8');
 let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
@@ -9,25 +5,27 @@ let data = JSON.parse(character_info);
 let communityDistricts = [];
 
 for(let i in data){
-  communityDistricts[i-1] = data[i].CDID;
+ communityDistricts[i-1] = data[i].CDID;
 }
 
 
 let index_html = ejs.render(index_template, {
-  filename: __dirname + '/views/index.ejs',
-  data: JSON.parse(character_info)
+ data: JSON.parse(character_info)
 });
 
-let micro_html = ejs.render(micro1, {
-  filename: __dirname + '/views/micro.ejs',
-  data: JSON.parse(character_info)
+let micro_html;
+
+for(let i = 0; i < communityDistricts.length; i++){
+micro_html = ejs.render(micro1, {
+ filename: __dirname + '/views/micro.ejs',
+ data: data[i+1]
 });
+fs.writeFileSync('build/micro' + communityDistricts[i]+ '.html', micro_html, 'utf8');
+}
 
 let about_html = ejs.render(about_template, {
-  filename: __dirname + '/views/about.ejs',
-  data: JSON.parse(character_info)
+ filename: __dirname + '/views/about.ejs',
 });
 
 fs.writeFileSync('build/index.html', index_html, 'utf8');
-fs.writeFileSync('build/micro.html', micro_html, 'utf8');
 fs.writeFileSync('build/about.html', about_html, 'utf8');
