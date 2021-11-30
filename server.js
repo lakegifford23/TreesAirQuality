75289676259af03ca12cb49a65d17087b1628fbc
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-11-30 12:00:06 -0500

    fixed the mistakes, this version will be fine to pull

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('build')); //greenlights all requests for files in the static folder
+app.use(express.static('docs')); //greenlights all requests for files in the static folder
 
 app.listen(port, () => {
   console.log(`data journalism server listening at http://localhost:${port}`)
