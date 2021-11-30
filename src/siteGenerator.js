c328804f46ebc787a07ffedc3dc1b385d28b20ba
nathalieperra22 <nathalie.perra22@trinityschoolnyc.org>
2021-11-29 22:12:11 -0500

    changing character to district_info
    but for some reason i am having an issue with micro sum on micro pages but when i fix it it doesn't access the data dynamically :(

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 
-let character_info = fs.readFileSync('data/CDdata.json', 'utf8');
+let district_info = fs.readFileSync('data/CDdata.json', 'utf8');
 let index_template = fs.readFileSync('src/views/index.ejs', 'utf8');
 let micro1 = fs.readFileSync('src/views/micro.ejs', 'utf8');
 let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
-let data = JSON.parse(character_info);
 let communityDistricts = [];
+
+let data = JSON.parse(district_info);
 
 for(let i in data){
  communityDistricts[i-1] = data[i].CDID;
@@ -16,22 +17,24 @@ for(let i in data){
 
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
- data: JSON.parse(character_info)
+  data: data
 });
 
 let micro_html;
 
-for(let i = 0; i < communityDistricts.length; i++){
+for(let i = 0; i < data.length; i++){
 micro_html = ejs.render(micro1, {
  filename: __dirname + '/views/micro.ejs',
- data: data[i+1]
+ data: data[i]
 });
+
 fs.writeFileSync('build/micro' + communityDistricts[i]+ '.html', micro_html, 'utf8');
+
 }
 
 let about_html = ejs.render(about_template, {
  filename: __dirname + '/views/about.ejs',
- data: JSON.parse(character_info)
+ data: data
 });
 
 fs.writeFileSync('build/index.html', index_html, 'utf8');
