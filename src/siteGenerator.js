b27fa71740fd23426a8b544c4185a44953738166
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-12-02 12:23:36 -0500

    working on doughnut chart again...

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -6,6 +6,8 @@ let district_info = fs.readFileSync('data/CDdata.json', 'utf8');
 let index_template = fs.readFileSync('src/views/index.ejs', 'utf8');
 let micro1 = fs.readFileSync('src/views/micro.ejs', 'utf8');
 let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
+let doughnutChart_template = fs.readFileSync('src/js/doughnutChart.js', 'utf8');
+
 let data = JSON.parse(district_info);
 let communityDistricts = [];
 
@@ -22,12 +24,11 @@ let index_html = ejs.render(index_template, {
 let micro_html;
 
 for(let i = 0; i < communityDistricts.length; i++){
-micro_html = ejs.render(micro1, {
- filename: __dirname + '/views/micro.ejs',
- data: JSON.parse(district_info),
- data2: data[i+1],
-
-});
+  micro_html = ejs.render(micro1, {
+   filename: __dirname + '/views/micro.ejs',
+   data: JSON.parse(district_info),
+   data2: data[i+1],
+  });
 fs.writeFileSync('docs/micro' + communityDistricts[i]+ '.html', micro_html, 'utf8');
 }
 
@@ -36,5 +37,7 @@ let about_html = ejs.render(about_template, {
  data: JSON.parse(district_info)
 });
 
+
 fs.writeFileSync('docs/index.html', index_html, 'utf8');
 fs.writeFileSync('docs/about.html', about_html, 'utf8');
+fs.writeFileSync('docs/doughnutChart.js', doughnutChart_template, 'utf8');
