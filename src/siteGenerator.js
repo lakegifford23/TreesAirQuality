6cf3b33e13f1c30346566b453ce58e03794791e7
lakegifford23 <56414210+lakegifford23@users.noreply.github.com>
2021-12-05 15:48:03 -0500

    map??

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -7,6 +7,7 @@ let index_template = fs.readFileSync('src/views/index.ejs', 'utf8');
 let micro1 = fs.readFileSync('src/views/micro.ejs', 'utf8');
 let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
 let doughnutChart_template = fs.readFileSync('src/js/doughnutChart.js', 'utf8');
+let map_template = fs.readFileSync('src/js/map.js', 'utf8');
 
 let data = JSON.parse(district_info);
 let communityDistricts = [];
@@ -15,10 +16,12 @@ for(let i in data){
  communityDistricts[i-1] = data[i].CDID;
 }
 
-
+//console.log(fs.readFileSync('src/js/geojson.json', 'utf8'));
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
- data: JSON.parse(district_info)
+ data: JSON.parse(district_info),
+ nyc: JSON.parse(fs.readFileSync('src/js/geojson.json', 'utf8')),
+ districtData: JSON.parse(fs.readFileSync('data/CDdata.json', 'utf8'))
 });
 
 let micro_html;
@@ -27,7 +30,7 @@ for(let i = 0; i < communityDistricts.length; i++){
   micro_html = ejs.render(micro1, {
    filename: __dirname + '/views/micro.ejs',
    data: JSON.parse(district_info),
-   data2: data[i+1],
+   data2: data[i+1]
   });
 fs.writeFileSync('docs/micro' + communityDistricts[i]+ '.html', micro_html, 'utf8');
 }
@@ -41,3 +44,4 @@ let about_html = ejs.render(about_template, {
 fs.writeFileSync('docs/index.html', index_html, 'utf8');
 fs.writeFileSync('docs/about.html', about_html, 'utf8');
 fs.writeFileSync('docs/doughnutChart.js', doughnutChart_template, 'utf8');
+fs.writeFileSync('docs/map.js', map_template, 'utf8');
